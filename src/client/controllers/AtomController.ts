7d0f135d5a1fb18f802622fd096fe3ae9bf09fc9
safazi <safazi@airmail.cc>
2024-08-04 14:56:04 -0500

    deps, fix config, atoms+sync

---
+++ src/client/controllers/AtomController.ts
@@ -0,0 +1,14 @@
+import { Controller, OnStart } from "@flamework/core";
+import { sync } from "@rbxts/charm";
+import * as atoms from "common/atoms";
+import { remotes } from "common/remotes";
+
+const client = sync.client({atoms})
+
+@Controller()
+export class AtomController implements OnStart {
+	onStart() {
+		remotes.atoms.sync.connect(payload => client.sync(payload));
+		remotes.atoms.init.fire()
+	}
+}
--- src/common/remotes.ts
+++ src/common/remotes.ts
@@ -1,3 +1,9 @@
-import { createRemotes } from "@rbxts/remo";
+import { SyncPayload } from "@rbxts/charm";
+import { Client, createRemotes, namespace, remote, Server } from "@rbxts/remo";
 
-export const remotes = createRemotes({});
+export const remotes = createRemotes({
+	atoms: namespace({
+		sync: remote<Client, [payload: SyncPayload<typeof import("common/atoms")>]>(),
+		init: remote<Server>()
+	})
+});
