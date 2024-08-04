4f412dd5e106ee681af4582763efa30e70470c7a
safazi <safazi@airmail.cc>
2024-08-04 15:14:41 -0500

    lint

--- src/client/controllers/AtomController.ts
+++ src/client/controllers/AtomController.ts
@@ -3,12 +3,12 @@ import { sync } from "@rbxts/charm";
 import * as atoms from "common/atoms";
 import { remotes } from "common/remotes";
 
-const client = sync.client({atoms})
+const client = sync.client({ atoms });
 
 @Controller()
 export class AtomController implements OnStart {
 	onStart() {
 		remotes.atoms.sync.connect(payload => client.sync(payload));
-		remotes.atoms.init.fire()
+		remotes.atoms.init.fire();
 	}
 }
