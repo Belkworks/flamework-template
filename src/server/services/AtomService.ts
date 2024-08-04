783a80972febe9ef1653ae652d79e74e92e7f2c3
safazi <safazi@airmail.cc>
2024-08-04 15:11:03 -0500

    lint

--- src/server/services/AtomService.ts
+++ src/server/services/AtomService.ts
@@ -3,14 +3,14 @@ import { sync } from "@rbxts/charm";
 import * as atoms from "common/atoms";
 import { remotes } from "common/remotes";
 
-const server = sync.server({atoms});
+const server = sync.server({ atoms });
 
 @Service()
 export class AtomService implements OnStart {
 	onStart() {
 		const syncRemote = remotes.atoms.sync;
 		server.connect((player, payload) => syncRemote.fire(player, payload));
-		
+
 		remotes.atoms.init.connect(player => server.hydrate(player));
 	}
 }
