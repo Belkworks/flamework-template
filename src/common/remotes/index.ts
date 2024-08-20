7dcaadab4b7c097fdf31e08588cd091b75891524
safazi <safazi@airmail.cc>
2024-08-20 16:43:06 -0500

    alias remote import

--- src/common/remotes/index.ts
+++ src/common/remotes/index.ts
@@ -1,6 +1,6 @@
 import { createRemotes } from "@rbxts/remo";
-import { atomRemotes } from "./namespaces/atoms";
+import { atomRemotes as atoms } from "./namespaces/atoms";
 
 export const remotes = createRemotes({
-	atoms: atomRemotes,
+	atoms,
 });
