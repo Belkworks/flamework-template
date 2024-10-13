2c87730dc28e0084284ad4bba28fd31de7143eee
safazi <safazi@airmail.cc>
2024-10-13 17:48:36 -0500

    fix sync type

--- src/common/remotes/namespaces/atoms.ts
+++ src/common/remotes/namespaces/atoms.ts
@@ -1,4 +1,4 @@
-import { SyncPayload } from "@rbxts/charm";
+import { SyncPayload } from "@rbxts/charm-sync";
 import { Client, namespace, remote, Server } from "@rbxts/remo";
 
 export const atomRemotes = namespace({
