105724e9e8c6f71eeaa3f87218cf573795aeefb1
safazi <safazics@gmail.com>
2025-01-12 18:53:52 -0600

    selective sync

--- src/server/services/AtomService.ts
+++ src/server/services/AtomService.ts
@@ -1,15 +1,46 @@
 import { OnStart, Service } from "@flamework/core";
-import CharmSync from "@rbxts/charm-sync";
+import CharmSync, { SyncPatch, SyncPayload } from "@rbxts/charm-sync";
 import * as atoms from "common/atoms";
 import { remotes } from "common/remotes";
 
 const server = CharmSync.server({ atoms });
+const syncRemote = remotes.atoms.sync;
+
+const filterPayload = (player: Player, payload: SyncPayload<typeof atoms>): SyncPayload<typeof atoms> | undefined => {
+	if (payload.type === "init")
+		return {
+			type: "init",
+			data: {
+				...payload.data,
+				sharedPlayerData: {
+					[player.Name]: payload.data.sharedPlayerData.get(player.Name),
+				} as unknown as ReturnType<typeof atoms.sharedPlayerData>,
+			},
+		};
+
+	const playerPatch = payload.data.sharedPlayerData?.get(player.Name);
+	const data = {
+		...payload.data,
+		sharedPlayerData: playerPatch
+			? ({ [player.Name]: playerPatch } as unknown as SyncPatch<ReturnType<typeof atoms.sharedPlayerData>>)
+			: undefined,
+	};
+
+	if (!next(data)[0]) return;
+
+	return {
+		type: "patch",
+		data,
+	};
+};
 
 @Service()
 export class AtomService implements OnStart {
 	onStart() {
-		const syncRemote = remotes.atoms.sync;
-		server.connect((player, payload) => syncRemote.fire(player, payload));
+		server.connect((player, payload) => {
+			const filtered = filterPayload(player, payload);
+			if (filtered) syncRemote.fire(player, filtered);
+		});
 
 		remotes.atoms.init.connect(player => server.hydrate(player));
 	}
